82a49f380bcf4879c603e898cbb1f4bc8de8a7e0
Ryohei Maeda <poemoepmaeda@yahoo.co.jp>
2021-01-30 13:24:23 +0900

    change generator code to multi axis ver

--- L6470_user.h
+++ L6470_user.h
@@ -13,11 +13,40 @@
 #define L6470_SPI_CH 0  // 0 or 1
 #define SPI_SPEED 1000000 // [Hz]
 
-#if defined (L6470_DAISY_CHAIN)
 #define L6470_DEV_NUM 2 
-#endif
 
-const union L6470_packet L6470_user_setting[PARAM_NUM] =
+const union L6470_packet L6470_user_setting[L6470_DEV_NUM][PARAM_NUM] =
+{// L6470_PARAM_addr,     setting[2],setting[1],setting[0]} //reset_val
+//Left justified, MSB first
+    {{REG_ABS_POS,       {0x00,   0x00,   0x00}}}, //0x000000
+    {{REG_EL_POS,        {0x00,   0x00,   0x00}}}, //0x000
+    {{REG_MARK,          {0x00,   0x00,   0x00}}}, //0x000000
+    {{REG_SPEED,         {0x00,   0x00,   0x00}}}, //dummy
+    {{REG_ACC,           {0x00,   0x8A,   0x00}}}, //0x08A
+    {{REG_DEC,           {0x00,   0x8A,   0x00}}}, //0x08A
+    {{REG_MAX_SPEED,     {0x00,   0x41,   0x00}}}, //0x041
+    {{REG_MIN_SPEED,     {0x00,   0x00,   0x00}}}, //0x000
+    {{REG_KVAL_HOLD,     {0x29,   0x00,   0x00}}}, //0x29
+    {{REG_KVAL_RUN,      {0x29,   0x00,   0x00}}}, //0x29
+    {{REG_KVAL_ACC,      {0x29,   0x00,   0x00}}}, //0x29
+    {{REG_KVAL_DEC,      {0x29,   0x00,   0x00}}}, //0x29
+    {{REG_INT_SPEED,     {0x04,   0x08,   0x00}}}, //0x0408
+    {{REG_ST_SLP,        {0x19,   0x00,   0x00}}}, //0x19 
+    {{REG_FN_SLP_ACC,    {0x29,   0x00,   0x00}}}, //0x29
+    {{REG_FN_SLP_DEC,    {0x29,   0x00,   0x00}}}, //0x29
+    {{REG_K_THERM,       {0x00,   0x00,   0x00}}}, //0x0
+    {{REG_ADC_OUT,       {0x00,   0x00,   0x00}}}, //dummy
+    {{REG_OCD_TH,        {0x08,   0x00,   0x00}}}, //0x8
+    {{REG_STALL_TH,      {0x40,   0x00,   0x00}}}, //0x40
+    {{REG_FS_SPD,        {0x00,   0x27,   0x00}}}, //0x027
+    {{REG_STEP_MODE,     {0x07,   0x00,   0x00}}}, //0x07
+    {{REG_ALARM_EN,      {0xFF,   0x00,   0x00}}}, //0xFF
+    {{REG_CONFIG,        {0x2E,   0x88,   0x00}}}, //0x2E88
+    {{REG_STATUS,        {0x00,   0x00,   0x00}}}//, //dummy
+//    {{REG_RESERVED_h1A,  {0x00,   0x00,   0x00}}}, //dummy
+//    {{REG_RESERVED_h1B,  {0x00,   0x00,   0x00}}}, //dummy
+
+},
 {// L6470_PARAM_addr,     setting[2],setting[1],setting[0]} //reset_val
 //Left justified, MSB first
     {{REG_ABS_POS,       {0x00,   0x00,   0x00}}}, //0x000000
--- generator.c
+++ generator.c
@@ -14,37 +14,49 @@ static int print_pkt(union L6470_packet pkt);
 static union L6470_packet generate_pkt(int enum_param,int32_t val);
 static union L6470_packet generate_pkt_with_percentage(int enum_param, int32_t percentage);
 
-
-
 int main (int argc, char** argv)
 {
     output_header();
 
+    for(int i = 0; i < L6470_DEV_NUM; i++){
 
-    print_pkt(gen_ABS_POS(USER_ABS_POS));
-    print_pkt(gen_EL_POS(USER_EL_POS));
-    print_pkt(gen_MARK(USER_MARK));
-    printf("    {{0x04,         {0x00,   0x00,   0x00}}}, //dummy\n");
-    print_pkt(gen_ACC(USER_ACC));
-    print_pkt(gen_DEC(USER_DEC));
-    print_pkt(gen_MAX_SPEED(USER_MAX_SPEED));
-    print_pkt(gen_MIN_SPEED(USER_MIN_SPEED));
-    print_pkt(gen_KVAL_HOLD(USER_KVAL_HOLD));
-    print_pkt(gen_KVAL_RUN(USER_KVAL_RUN));
-    print_pkt(gen_KVAL_ACC(USER_KVAL_ACC));
-    print_pkt(gen_KVAL_DEC(USER_KVAL_DEC));
-    print_pkt(gen_INT_SPEED(USER_INT_SPEED));
-    print_pkt(gen_ST_SLP(USER_ST_SLP));
-    print_pkt(gen_FN_SLP_ACC(USER_FN_SLP_ACC));
-    print_pkt(gen_FN_SLP_DEC(USER_FN_SLP_DEC));
-    print_pkt(gen_K_THERM(USER_K_THERM));
-    printf("    {{0x12,       {0x00,   0x00,   0x00}}}, //dummy\n");
-    print_pkt(gen_OCD_TH(USER_OCD_TH));
-    print_pkt(gen_STALL_TH(USER_STALL_TH));
-    print_pkt(gen_FS_SPD(USER_FS_SPD));
-    print_pkt(gen_STEP_MODE(USER_STEP_MODE));
-    print_pkt(gen_ALARM_EN(USER_ALARM_EN));
-    print_pkt(gen_CONFIG(USER_CONFIG));
+        output_packetStart();
+
+        print_pkt(gen_ABS_POS(USER_ABS_POS));
+        print_pkt(gen_EL_POS(USER_EL_POS));
+        print_pkt(gen_MARK(USER_MARK));
+        printf("    {{0x04,         {0x00,   0x00,   0x00}}}, //dummy\n");
+        print_pkt(gen_ACC(USER_ACC));
+        print_pkt(gen_DEC(USER_DEC));
+        print_pkt(gen_MAX_SPEED(USER_MAX_SPEED));
+        print_pkt(gen_MIN_SPEED(USER_MIN_SPEED));
+        print_pkt(gen_KVAL_HOLD(USER_KVAL_HOLD));
+        print_pkt(gen_KVAL_RUN(USER_KVAL_RUN));
+        print_pkt(gen_KVAL_ACC(USER_KVAL_ACC));
+        print_pkt(gen_KVAL_DEC(USER_KVAL_DEC));
+        print_pkt(gen_INT_SPEED(USER_INT_SPEED));
+        print_pkt(gen_ST_SLP(USER_ST_SLP));
+        print_pkt(gen_FN_SLP_ACC(USER_FN_SLP_ACC));
+        print_pkt(gen_FN_SLP_DEC(USER_FN_SLP_DEC));
+        print_pkt(gen_K_THERM(USER_K_THERM));
+        printf("    {{0x12,       {0x00,   0x00,   0x00}}}, //dummy\n");
+        print_pkt(gen_OCD_TH(USER_OCD_TH));
+        print_pkt(gen_STALL_TH(USER_STALL_TH));
+        print_pkt(gen_FS_SPD(USER_FS_SPD));
+        print_pkt(gen_STEP_MODE(USER_STEP_MODE));
+        print_pkt(gen_ALARM_EN(USER_ALARM_EN));
+        print_pkt(gen_CONFIG(USER_CONFIG));
+
+        output_packetEnd();
+        if((L6470_DEV_NUM - 1) == i)
+        {
+            printf(";");
+        }else
+        {
+            printf(",");
+        }
+        
+    }
 
     output_footer();
 
@@ -52,48 +64,50 @@ int main (int argc, char** argv)
 
 }
 
-static int print_pkt(union L6470_packet pkt)
+static void print_pkt(union L6470_packet pkt)
 {
     printf("    {{\t0x%02x,\t{0x%02x,\t0x%02x,\t0x%02x}}},\n",pkt.value8b[0], pkt.value8b[1], pkt.value8b[2], pkt.value8b[3]);
-    return 0;
 }
 
-static int output_header()
+static void output_header()
 {
-printf("#include \"L6470.h\"\n");
-printf("#ifndef __L6470_USER_HEADER__\n");
-printf("#define __L6470_USER_HEADER__\n");
-printf("\n");
-printf("//#define L6470_DAISY_CHAIN\n");
-printf("#define L6470_SPI_CH 0  // 0 or 1\n");
-printf("#define SPI_SPEED 1000000 // [Hz]\n");
-printf("\n");
-printf("#if defined (L6470_DAISY_CHAIN)\n"); 
-printf("#define L6470_DEV_NUM 2\n");
-printf("#endif\n");
-printf("\n");
-printf("const union L6470_packet L6470_user_setting[PARAM_NUM] =\n");
-printf("{// L6470_PARAM_addr,     setting[2],setting[1],setting[0]} //reset_val\n");
-printf("//Left justified, MSB first\n");
-printf("\n");
-
-
-return 0;
+    printf("#include \"L6470.h\"\n");
+    printf("#ifndef __L6470_USER_HEADER__\n");
+    printf("#define __L6470_USER_HEADER__\n");
+    printf("\n");
+    printf("//#define L6470_DAISY_CHAIN\n");
+    printf("#define L6470_SPI_CH 0  // 0 or 1\n");
+    printf("#define SPI_SPEED 1000000 // [Hz]\n");
+    printf("\n");
+    // printf("#if defined (L6470_DAISY_CHAIN)\n"); 
+    printf("#define L6470_DEV_NUM (%d)\n",L6470_DEV_NUM);
+    // printf("#endif\n");
+    printf("\n");
+    printf("const union L6470_packet L6470_user_setting[L6470_DEV_NUM][PARAM_NUM] =\n");
 }
 
-static int output_footer()
+static void output_packetStart()
 {
-// printf("    //Dummy READONLY param &  RESERVED param\n");
-printf("    {{0x19,        {0x00,   0x00,   0x00}}}//, //dummy\n");
-printf("//    {{L6470_param[enum_L6470_RESERVED_h1A].addr,  {0x00,   0x00,   0x00}}}, //dummy\n");
-printf("//    {{L6470_param[enum_L6470_RESERVED_h1B].addr,  {0x00,   0x00,   0x00}}}, //dummy\n");
-printf("\n");
-printf("};\n");
+    printf("{// L6470_PARAM_addr,     setting[2],setting[1],setting[0]} //reset_val\n");
+    printf("//Left justified, MSB first\n");
+    printf("\n");
+}
 
-printf("#endif\n");
+static void output_packetEnd()
+{
+    // printf("    //Dummy READONLY param &  RESERVED param\n");
+    printf("    {{0x19,        {0x00,   0x00,   0x00}}}//, //dummy\n");
+    printf("//    {{L6470_param[enum_L6470_RESERVED_h1A].addr,  {0x00,   0x00,   0x00}}}, //dummy\n");
+    printf("//    {{L6470_param[enum_L6470_RESERVED_h1B].addr,  {0x00,   0x00,   0x00}}}, //dummy\n");
+    printf("\n");
+    printf("}");
+}
 
+static void output_footer()
+{
+    printf(";\n\n");
+    printf("#endif\n");
 
-return 0;
 }
 
 
@@ -101,7 +115,7 @@ return 0;
 union L6470_packet gen_ABS_POS(int32_t abs_pos)
 {
 #if defined (L6470_PRINT_MESSAGE)
-    if( (abs_pos > (int32_t)(pow(2,21)-1)) | (abs_pos < (int32_t)((-1)*pow(2,21))) )
+    if( (abs_pos > (int32_t)(pow(2,21)-1)) | (abs_pos < (int32_t)((-1)*pow(2,21))) ) 
         printf("// %s %s abs_pos is over/under flow\n",L6470_PRINT_HEADER,L6470_PRINT_CAUTION);
 #endif
     int32_t val = abs_pos & (int32_t)(pow(2,21)-1);
