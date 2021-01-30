ed3f145149f4a729023ab911457f8b98a0383fb7
Ryohei Maeda <poemoepmaeda@yahoo.co.jp>
2021-01-31 06:13:50 +0900

    adc

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(int argc, char** argv)
 
 
     int pos_aim,pos;
-    L6470_packet pkt_r = L6470_MoveRun(DIR_FWD,25000000);
+    L6470_packet pkt_r = L6470_MoveRun(DIR_FWD,2500000);
     L6470_packet pkt_l = pkt_r;
     char c = 0;
     c = getchar();
