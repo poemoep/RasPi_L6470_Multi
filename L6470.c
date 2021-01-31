45f6aab804653b8bbad5311f6fbf0811ca9513f9
Ryohei Maeda <poemoepmaeda@yahoo.co.jp>
2021-01-31 12:28:55 +0900

    bug fix

--- L6470.c
+++ L6470.c
@@ -311,7 +311,7 @@ int L6470_rw_multi(L6470_packet *pkt,int len, const char* msg, ...)
                                     input_pkt[itr]->value8b[1]
                                     input_pkt[itr]->value8b[2]
                                     input_pkt[itr]->value8b[3]
-                                    )
+                                    );
     }
 
     printf("summarize pkts to array\n");
