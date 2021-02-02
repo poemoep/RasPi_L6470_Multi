3051218063df1732014cbe267f4714b8d330948b
Ryohei Maeda <poemoepmaeda@yahoo.co.jp>
2021-02-02 23:12:08 +0900

    edit main function

--- main.c
+++ main.c
@@ -11,100 +11,206 @@ int main(int argc, char** argv)
 
     L6470_init();
 
-
-    int pos_aim,pos;
-    L6470_u_packet pkt_r = L6470_MoveRun(DIR_FWD,2500000);
-    L6470_u_packet pkt_l = pkt_r;
+    L6470_DATA_ARRAY d;
+    L6470_ABSPOS_T pos;
     char c = 0;
+
+    d.dev[0] = L6470_MoveRun(DIR_FWD,256000);
+    d.dev[1] = L6470_MoveRun(DIR_FWD,256000);
     c = getchar();
-    // pos = L6470_GetAbsPos();
-    L6470_rw_multi(&pkt_r,4,NULL,&pkt_l,4,NULL);
-    
+    L6470_rw_multi(&d, "MoveRun_TEST_1");
     c = getchar();
     sleep(5);
-    pos = L6470_GetAbsPos();
-    L6470_MoveRun(DIR_FWD, 5120000);
-    sleep(5);
-    pos = L6470_GetAbsPos();
-    
-    
-    
-    L6470_StopSoft();
-    sleep(2);
     pos = L6470_GetAbsPos();
 
-    L6470_MoveRun(DIR_RVS, 1600000);
+    d.dev[0] = L6470_MoveRun(DIR_FWD,512000);
+    d.dev[1] = L6470_MoveRun(DIR_RVS,512000);
+    c = getchar();
+    L6470_rw_multi(&d, "MoveRun_TEST_2");
+    c = getchar();
     sleep(5);
     pos = L6470_GetAbsPos();
-    L6470_StopSoft();
-    sleep(2);
+    
+    
+    
+    d.dev[0] = L6470_StopSoft();
+    d.dev[1] = L6470_StopSoft();
+    L6470_rw_multi(&d, "MoveRun_StopSoft");
+    sleep(5);
+    pos = L6470_GetAbsPos();
+
+    d.dev[0] = L6470_MoveRun(DIR_RVS,1024000);
+    d.dev[1] = L6470_MoveRun(DIR_FWD,1024000);
+    c = getchar();
+    L6470_rw_multi(&d, "MoveRun_TEST_3");
+    c = getchar();
+    sleep(5);
+    pos = L6470_GetAbsPos();
+
+
+    d.dev[0] = L6470_StopSoft();
+    d.dev[1] = L6470_StopSoft();
+    c = getchar();
+    L6470_rw_multi(&d, "MoveRun_StopSoft");
+    c = getchar();
+    sleep(5);
     pos = L6470_GetAbsPos();
     
     
-    L6470_MoveStepClock(DIR_FWD);
+    d.dev[0] = L6470_MoveStepClock(DIR_FWD);
+    d.dev[1] = L6470_nop(1);
+    c = getchar();
+    L6470_rw_multi(&d, "MoveStepClock_nop");
+    c = getchar();
     sleep(3);
     pos = L6470_GetAbsPos();
-    L6470_MoveStepClock(DIR_RVS);
+
+    d.dev[0] = L6470_nop(1);
+    d.dev[1] = L6470_MoveStepClock(DIR_RVS);
+    c = getchar();
+    L6470_rw_multi(&d, "nop_MoveStepClock");
+    c = getchar();
     sleep(3);
     pos = L6470_GetAbsPos();
 
     for(int i = 0; i<1; i++){
-        L6470_MoveStep(DIR_FWD,200);
+        d.dev[0] = L6470_MoveStep(DIR_FWD,2000);
+        d.dev[1] = L6470_MoveStep(DIR_RVS,4000);
+        c = getchar();
+        L6470_rw_multi(&d, "MoveStep");
+        c = getchar();
         sleep(1);
     	pos = L6470_GetAbsPos();
-        L6470_MoveStep(DIR_RVS,200);
+
+        d.dev[0] = L6470_MoveStep(DIR_RVS,4000);
+        d.dev[1] = L6470_MoveStep(DIR_FWD,2000);
+        c = getchar();
+        L6470_rw_multi(&d, "MoveStep");
+        c = getchar();
         sleep(1);
     	pos = L6470_GetAbsPos();
     }
     sleep(3);
 
-    pos_aim=10000;
-    pos = pos_aim + 1;
+    int pos_aim[L6470_DEV_NUM];
+
+    for(int itr = 0; itr < L6470_DEV_NUM; itr++){
+        pos_aim[itr]=10000;
+        pos_now[itr] = pos_aim[itr] + 1;
+
+    }
     
-    L6470_MoveGoTo(pos_aim);
-    while(pos != pos_aim){
+    d.dev[0] = L6470_MoveGoto(pos_aim[0]);
+    d.dev[1] = L6470_MoveGoTo(pos_aim[1]);
+    c = getchar();
+    L6470_rw_multi(&d, "MoveGoTo");
+
+    char flag = 0;
+    while(flag == 0){
    // for(int l = 0 ; l < 20; l++){
-	pos = L6470_GetAbsPos();
+	    pos = L6470_GetAbsPos();
+        for(int itr = 0; itr <L6470_DEV_NUM; itr++){
+            if(pos.dev[itr] == pos_aim[itr]){
+                flag++; 
+            }else{
+                flag = 0;
+            }
+        }
+        if(flag != L6470_DEV_NUM) flag = 0;    
         sleep(1);
     }
 
     
    // L6470_MoveStep(DIR_RVS,200);
     sleep(1);
+
+    pos_aim[0] = -1;
+    pos_aim[1] = 20000;    
     
-    pos_aim=-1;
-    pos = pos_aim + 1;
-    
-    L6470_MoveGoToDir(DIR_RVS, pos_aim);
-    while(pos != pos_aim){
-    //for(int l = 0 ; l < 20; l++){
-	pos = L6470_GetAbsPos();
+    d.dev[0] = L6470_MoveGoToDir(DIR_RVS,pos_aim[0]);
+    d.dev[1] = L6470_MoveGoToDir(DIR_FWD,pos_aim[1]);
+    c = getchar();
+    L6470_rw_multi(&d, "MoveGoToDir");
+
+    flag = 0;
+    while(flag == 0){
+   // for(int l = 0 ; l < 20; l++){
+	    pos = L6470_GetAbsPos();
+        for(int itr = 0; itr <L6470_DEV_NUM; itr++){
+            if(pos.dev[itr] == pos_aim[itr]){
+                flag++; 
+            }else{
+                flag = 0;
+            }
+        }
+        if(flag != L6470_DEV_NUM) flag = 0;    
         sleep(1);
     }
 
-    int mark[2] = {1,2};   
-    L6470_MoveGoToUntil(ACT_POS_TO_MARK,DIR_FWD,10000);
-    while(mark[0] != mark[1]){
-	pos = L6470_GetAbsPos();
-	mark[0] = mark[1];
-	mark[1] = pos;
-        sleep(1);
-    }
-    pos = 100;
-    L6470_GoHome();
-    while(pos != 0){
-	pos = L6470_GetAbsPos();
+    int mark[2][2] = {{1,2},{1,2}}; 
+
+    d.dev[0] = L6470_MoveGoToUntil(ACT_POS_TO_MARK,DIR_FWD,20000);
+    d.dev[1] = L6470_MoveGoToUntil(ACT_POS_TO_MARK,DIR_RVS,80000);
+    c = getchar();
+    L6470_rw_multi(&d, "MoveGoToUntil");
+
+    while(flag == 0){
+    	pos = L6470_GetAbsPos();
+        for(int itr = 0; itr <L6470_DEV_NUM; itr++){
+    	    mark[itr][0] = mark[itr][1];
+	        mark[itr][1] = pos.dev[itr];
+
+            if(mark[itr][0] == mark[itr][1]){
+                flag++;
+            }else{
+                flag = 0:
+            }
+        }
+        if(flag != L6470_DEV_NUM) flag = 0;
         sleep(1);
     }
 
-
-   L6470_GoMark();
-   while(pos != mark[0]){
-	pos = L6470_GetAbsPos();
+    flag = 0;
+    d.dev[0] = L6470_GoHome();
+    d.dev[1] = L6470_GoHome();
+    c = getchar();
+    L6470_rw_multi(&d, "MoveGoHome");
+    while(flag == 0){
+    	pos = L6470_GetAbsPos();
+        for(int itr = 0; itr <L6470_DEV_NUM; itr++){
+    	    if(pos.dev[itr] == 0){
+                flag++;
+            }else{
+                flag = 0:
+            }
+        }
+        if(flag != L6470_DEV_NUM) flag = 0;
         sleep(1);
     }
 
-    L6470_HiZSoft();
+    flag = 0;
+    d.dev[0] = L6470_GoMark();
+    d.dev[1] = L6470_GoMark();
+    c = getchar();
+    L6470_rw_multi(&d, "MoveGoMark");
+
+    while(flag == 0){
+    	pos = L6470_GetAbsPos();
+        for(int itr = 0; itr <L6470_DEV_NUM; itr++){
+            if(mark[itr][0] == pos.dev[itr]){
+                flag++;
+            }else{
+                flag = 0:
+            }
+        }
+        if(flag != L6470_DEV_NUM) flag = 0;
+        sleep(1);
+    }
+
+    d.dev[0] = L6470_HiZHard();
+    d.dev[1] = L6470_HiZHard();
+    c = getchar();
+    L6470_rw_multi(&d, "HiZHard");
     
     return 0;
 }
